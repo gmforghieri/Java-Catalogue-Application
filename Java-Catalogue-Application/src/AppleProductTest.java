ba423ac967f11a98baf24214a3e855edf955e4ad
Gian Marco Forghieri <forghierigianmarco@gmail.com>
2019-02-09 00:39:07 +0100

    IntelliJ

--- Java-Catalogue-Application/src/AppleProductTest.java
+++ Java-Catalogue-Application/src/AppleProductTest.java
@@ -1,10 +1,9 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 import java.util.Scanner;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 
 class AppleProductTest {
--- Java-Catalogue-Application/src/IphoneTest.java
+++ Java-Catalogue-Application/src/IphoneTest.java
@@ -1,7 +1,7 @@
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
-import static org.junit.Assert.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 
 class IphoneTest {
